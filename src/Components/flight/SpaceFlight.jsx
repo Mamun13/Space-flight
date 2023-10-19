9a4246984cd4ffad5396398d95f4f503035799d3
Al Mamun <mamuncse82@gmail.com>
2023-10-19 20:06:59 +0600

    environment setup

---
+++ src/Components/flight/SpaceFlight.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+const SpaceFlight = () => {
+  return (
+    <>
+
+      <section>
+        <Container>
+          <h1 className="text-capitalize text-center mt-5">Spaceflight details</h1>
+          <p className="text-center">
+            Find out the elaborate features of all past big spaceflights.
+          </p>
+          
+        </Container>
+      </section>
+    </>
+  );
+};
+
+export default SpaceFlight;
